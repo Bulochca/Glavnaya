c422bd344b96b5f4cfbe7db00e93a1afea2d3091
Bulochca <nik.sadovshikov@gmail.com>
2020-10-19 17:11:32 +0500

    Авторизация

---
+++ authorization.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Вход</title>
+</head>
+<body>
+	<form method="post">
+		<div>
+	        <label>ЭЛЕКТРОННЫЙ АДРЕС:</label>
+	        <input type="email" id="Email" name="Email"><br>
+      	</div>
+      	<div>
+	        <label>ПАРОЛЬ:</label>
+	        <input type="password" id="password" name="password"><br>
+      	</div>
+      	<div>
+      		<a href="registration.php">Зарегистрироваться</a>
+      	</div>
+	</form>
+</body>
+</html>
